fbd7b08f36c925e6d2f64aed3ff2fdece1869be3
Zeeshan Abid <zeesh.rocky@gmail.com>
2016-07-09 13:01:02 +0530

    Add files via upload

---
+++ assign4.java
@@ -0,0 +1,15 @@
+class Exception
+{
+   public static void main(String args[])
+   {
+      try{
+	 String str="easysteps2buildwebsite";
+	 System.out.println(str.length());;
+	 char c = str.charAt(0);
+	 c = str.charAt(40);
+	 System.out.println(c);
+      }catch(StringIndexOutOfBoundsException e){
+	  System.out.println("StringIndexOutOfBoundsException!!");
+       }
+   }
+}
